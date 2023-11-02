3a1ddb9153076ac486b9b0fb755b10570ca2feb0
Hemanth Sai <hemanth.sai@mindsetconsulting.com>
2023-11-02 09:21:41 +0000

    changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,5 +1,3 @@
-
-
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/model/json/JSONModel"
@@ -7,60 +5,48 @@ sap.ui.define([
   "use strict";
 
   return Controller.extend("com.mindset.project7.controller.View1", {
-    onInit: function () {
-      var oModel = new JSONModel();
-      oModel.loadData("/model/data.json");
+      onInit: function () {
+          var oModel = new JSONModel();
+          oModel.attachRequestCompleted(function () {
+              this.getView().setModel(oModel);
+              oModel.setProperty("/filteredProducts", oModel.getProperty("/products"));
+          }.bind(this));
+          oModel.loadData("/model/data.json");
+      },
 
-      // Attach a request completed handler to ensure the data is loaded before binding
-      oModel.attachRequestCompleted(function () {
-        this.getView().setModel(oModel);
-        oModel.setProperty("/filteredProducts", oModel.getProperty("/products"));
-      }.bind(this));
-      
-    },
+      onAddToCartPress: function () {
+          var oModel = this.getView().getModel();
+          var aProducts = oModel.getProperty("/products");
+          var aCart = oModel.getProperty("/cart");
 
-    onProductSelect: function (oEvent) {
-      var oItem = oEvent.getSource().getBindingContext().getObject();
-      oItem.selected = oEvent.getParameter("selected");
-    },
+          var selectedProducts = aProducts.filter(function (product) {
+              return product.selected;
+          });
 
-    onAddToCartPress: function () {
-      var oModel = this.getView().getModel();
-      var aProducts = oModel.getProperty("/products");
-      var aCart = oModel.getProperty("/cart");
+          if (selectedProducts.length === 0) {
+              alert("Select at least one item");
+          } else {
+              aCart = aCart.concat(selectedProducts);
+              oModel.setProperty("/cart", aCart);
 
-      // Add selected products to the cart
-      var selectedProducts = aProducts.filter(function (product) {      // used Filter Function
-        return product.selected;
-      });
-      if (selectedProducts.length === 0) {
-        alert(" Select Atleast one item");
+              aProducts.forEach(function (product) {
+                  product.selected = false;
+              });
+
+              oModel.refresh();
+          }
+      },
+
+      onSearch: function (oEvent) {
+          var oModel = this.getView().getModel();
+          var sSearchValue = oEvent.getParameters().newValue;
+          var aProducts = oModel.getProperty("/products");
+
+          var filteredProducts = aProducts.filter(function (product) {
+              return product.name.toLowerCase().includes(sSearchValue.toLowerCase());
+          });
+
+          oModel.setProperty("/filteredProducts", filteredProducts);
       }
-      else {
-        aCart = aCart.concat(selectedProducts);
-        oModel.setProperty("/cart", aCart);
-
-        // Deselect selected products
-        aProducts.forEach(function (product) {
-          product.selected = false;
-        });
-
-        oModel.refresh();
-      }
-
-    },
-
-    onSearch: function (oEvent) {
-      var oModel = this.getView().getModel();
-      var sSearchValue = oEvent.getParameter("query");
-      var aProducts = oModel.getProperty("/products");
-
-      // Filter products based on the search value
-      var filteredProducts = aProducts.filter(function (product) {
-        return product.name.toLowerCase().includes(sSearchValue.toLowerCase());    // used String Function
-      });
-
-      oModel.setProperty("/filteredProducts", filteredProducts);
-    }
   });
 });
